8a958b200d31a016d047d047103e87d87adbe064
XobazJr <MR.TeerapatKotanartChannel@Gmail.COM>
2024-10-19 16:35:36 +0700

    Update heap.cpp
    ✅ Fix and update heap code

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 class max_heap{
 	public:
 		vector<int> heap;
-		
-	void heapify_up(int index){
-		if(index && heap[parent(index)] < heap[index]){
-			swap(heap[index],heap[parent(index)]);
-			heapify_up(parent(index));
-		}
+	
+	void heapify_up(){
+		make_heap(heap.begin(),heap.end());
 	}
 	
 	void heapify_down(int index){
-		int left = left_child(index);
-		int right = right_child(index);
+		int left = 2 * index + 1;
+		int right = 2 * index + 2;
 		int largest = index;
 		
 		if(left < heap.size() && heap[left] > heap[largest]){
@@ -28,12 +26,12 @@ class max_heap{
 			swap(heap[index],heap[largest]);
 			heapify_down(largest);
 		}
-		
+		 
 	}
 	
 	void add(int data){
 		heap.push_back(data);
-		heapify_up(heap.size() - 1);
+		heapify_up();
 	}
 	
 	void remove(){
@@ -41,29 +39,17 @@ class max_heap{
 			int remove_data = heap[0];
 			heap[0] = heap.back();
 			heap.pop_back();
-			heapify_down(0);
 			cout<<remove_data<<endl;
+			heapify_down(0);
 		}
 	}
 	
-	int print(){
+	void print(){
 		if(!heap.empty()){
 			for(int i=0;i<heap.size();i++){
 				cout<<heap[i]<<" ";
 			}cout<<endl;
 		}
-	}
-	
-	int parent(int index){
-		return (index - 1) / 2;
-	}
-	
-	int left_child(int index){
-		return 2 * index + 1;
-	}
-	
-	int right_child(int index){
-		return 2 * index + 2;
 	}
 };
 
